b9158d010ef5b7e5465aa5b75f493e94f7d542a7
Hai Pham <repperpep@gmail.com>
2020-07-13 16:31:40 +0300

    fix slider logic

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
             @endfor
         </ol>
         <div class="carousel-inner">
-            @if ($blogs->count() > 5)
+            @if ($blogs->count() >= 5)
                 @for ($i = 0; $i < 5; $i++)
                     <a  @if ($i===0) class="carousel-item active"
                         @else class="carousel-item" 
@@ -67,7 +67,7 @@
                             <a href="#">{{ $blog->user->name }}</a>
                         </span>
                     </p>
-                    <p class="card-text mb-0">{{$blog->blogText}}</p>
+                    <p class="card-text mb-0">{!!$blog->blogText!!}</p>
                     <a class="card-link" href="{{ "/blog/$blog->id" }}">Learn more
                         <svg class="bi bi-arrow-right" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fill-rule="evenodd" d="M10.146 4.646a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708-.708L12.793 8l-2.647-2.646a.5.5 0 0 1 0-.708z" />
